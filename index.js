4ef943cff08740d52625453ba9fadecefc12ca56
MohammedAlMaqbli <Mohammed.AlMaqbli@gmail.com>
2019-09-26 00:01:01 -0700

    Add for and while loops

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(writeCardsArray, name) {
+    let thankYouMessage = [];
+    for (let index = 0; index < writeCardsArray.length; index++) {
+        thankYouMessage.push(  "Thank you, " + writeCardsArray[index] + ", for the wonderful " + name + " gift!");
+
+    }
+    return thankYouMessage;
+}
+function countdown(postiveNumber) {
+    while (postiveNumber> -1 ) {
+        console.log(postiveNumber);
+        postiveNumber--;
+    }
+}
+writeCards(["Lisa", "Kaitlin", "Jan"], "surprise gift")
+
+
+

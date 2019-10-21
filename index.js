40666515f94546225724791fd1d19554ba00ef7b
Firas Al-Azizy <firasazizy614@gmail.com>
2019-10-21 01:35:01 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(input,event){
+    let p=[];
+for(let i=0;i<input.length;i++){
+   p.push(`Thank you, ${input[i]}, for the wonderful ${event} gift!`);
+
+}
+return p;
+}
+function countdown(input){
+    for (let countdown = input; countdown >=0; countdown--) {
+        console.log(countdown);
+      }
+}

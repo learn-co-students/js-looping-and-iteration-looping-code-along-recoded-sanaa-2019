0be2335dc76f5ecb9d01ebf801be045f3d5f236e
AISHA-PRO <aisha1rashed@gmail.com>
2019-10-22 12:19:47 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+const nameArr=["Ada", "Brendan", "Ali"];
+const event="birthday";
+function writeCards(names, event)
+{
+let thankMsg=[];
+    for (let i=0; i < names.length; i++)
+    {
+        thankMsg.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankMsg;
+}
+
+function countdown(num)
+{
+    while(num>=0)
+    {
+        console.log(num--);
+    }
+}
+writeCards(nameArr, "event");
+countdown(10);
+

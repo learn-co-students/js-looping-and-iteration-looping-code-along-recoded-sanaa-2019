51316adc9bef6fc969e5055b378d4d0636b1c0ba
Ahmed Hassan <ahmedalhassany1@gmail.com>
2019-10-23 21:45:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(names,e)
+{
+  let arr=[];
+  for(let i=0;i<names.length;i++)
+  {
+    arr[i]=`Thank you, ${names[i]}, for the wonderful ${e} gift!`;
+  }
+  return arr;
+}
+
+function countdown(n)
+{
+  while(n>=0)
+  {
+    console.log(n);
+    n--;
+  }
+}

89403e8a8e46b6e674492af14a29cd26d24147fb
TaghreedNageeb <taghreed.n.d2018@gmail.com>
2019-11-01 10:47:44 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+const Array=['Lisa','Kaitlin','Jan'];
+
+function writeCards(Array){
+    for (let i=0;i<Array.length;i++){
+        console.log('Thank you, ' +Array[i]+ ', for the wonderful surprise gift!')
+        Array[i]='Thank you, ' +Array[i]+ ', for the wonderful surprise gift!';
+    }
+
+    return Array;
+}
+writeCards(Array);
+console.log(Array);
+
+let inputNum = 10;
+
+function countDown(inputNum){
+    while (inputNum>=0){
+        console.log(inputNum);
+        inputNum--;
+    }
+}
+countDown(inputNum);

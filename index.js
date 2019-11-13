c02f3aeee07b2e9f618c5848726e4c514cf2f69a
Sarah Alafif <sar1997yah@gmail.com>
2019-11-13 17:21:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+let gifts =["Ada", "Brendan", "Ali"];
+let fo="birthday";
+
+function writeCards (gifts,fo) {
+  let array=[];
+
+  for(let i=0;i<gifts.length;i++){
+   array.push(`Thank you, ${gifts[i]}, for the wonderful ${fo} gift!`);
+   }     
+  
+return array;
+}
+
+function countdown(cound){
+while (cound > 0 ) {
+  console.log(cound);
+  cound --;
+}
+}

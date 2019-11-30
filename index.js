12015516f2964c9f445e28df0f641fb0e0a8fed6
Hana'a Eskander <hanaa.eskander95@gmail.com>
2019-11-30 19:51:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const person = ["Lisa", "Kaitlin", "Jan"];
+
+function writeCards(person,event){
+  const messArr = [];
+  for(let i = 0; i < person.length; i++){
+    messArr.push(`Thank you, ${person[i]}, for the wonderful ${event} gift!`);
+  }
+  return messArr;
+}
+writeCards(messArr, "surprise");
+
+function countDown(digit){
+  while (digit >= 0) {
+    console.log(digit);
+    digit--;
+  }
+}
+
+countDown(15);
